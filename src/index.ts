4ec144a1c87bbd98b5cd10f0b08bb14731ecaa72
Evgenia Golubeva <evgenia.m.golubeva@gmail.com>
2022-09-23 16:49:46 +0200

    remove connection test

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import { Request, Response, NextFunction } from 'express';
 import "reflect-metadata";
 import { createContact, createUser, loginUser } from './restAPI/endpoints';
 import dotenv from "dotenv";
@@ -9,19 +8,19 @@ import { TokenManager } from './authentication/TokenManager';
 const app = express();
 dotenv.config();
 
+// connect to the user database
 UserDatabase.initialize();
 
 app.use(express.json());
 
-app.get('', (req: Request, res: Response) => {
-    res.status(200);
-    res.json({running: true});
-})
+// create a new contact, only for authenticated users
 
 app.post('/contacts', TokenManager.authenticationMiddleware, createContact);
 
+// register a new user account
 app.post('/register', createUser);
 
+// login existing user
 app.post('/login', loginUser);
 
 app.listen(process.env.PORT, () => {
